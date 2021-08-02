936019b800e9092a5d57780b879b3677319c34b5
dionjwa1 <dionjwa@gmail.com>
2021-08-02 15:26:28 -0700

    created header

---
+++ src/components/storefront/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <h1> World's Greatest Online Store </h1>
+        </div>
+    )
+}
+
+export default Header;
