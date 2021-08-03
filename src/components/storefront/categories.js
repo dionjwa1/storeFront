56a7a63774600ba9037552ce6c31b19788db5d7e
dionjwa1 <dionjwa@gmail.com>
2021-08-02 17:35:20 -0700

    updated footer, cats, and header

---
+++ src/components/storefront/categories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Categories() {
+    return (
+        <div className='container'>
+            <h3 className='title'> Browse Our Categories </h3>
+            <ul className='list'></ul>
+            <li>Games</li>
+            <li>Books</li>
+            <li>Spiders</li>
+
+            </div>
+
+    )
+}
+
+export default Categories;
